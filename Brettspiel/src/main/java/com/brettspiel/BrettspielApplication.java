9bf2cf6af09ef03324c00706e08231b0ec3fd841
Joaquin <36995550+Joaquin26@users.noreply.github.com>
2019-05-25 15:54:51 -0500

    Initial commit

---
+++ Brettspiel/src/main/java/com/brettspiel/BrettspielApplication.java
@@ -0,0 +1,13 @@
+package com.brettspiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrettspielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrettspielApplication.class, args);
+	}
+
+}
