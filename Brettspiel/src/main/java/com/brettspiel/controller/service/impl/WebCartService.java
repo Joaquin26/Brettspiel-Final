cd2dae8d5dcd86c055b03e260be17c94fb0501ff
Daniel Iglesias <ri.da.igle.es@gmail.com>
2019-06-28 15:01:55 -0500

    Ya se guarda el carrito :D

--- Brettspiel/src/main/java/com/brettspiel/controller/service/impl/WebCartService.java
+++ Brettspiel/src/main/java/com/brettspiel/controller/service/impl/WebCartService.java
@@ -1,8 +1,11 @@
 package com.brettspiel.controller.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.brettspiel.model.WebCartDetail;
+import com.brettspiel.model.repository.IWebCartDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +18,8 @@ public class WebCartService implements IWebCartService {
 
 	@Autowired
 	private IWebCartRepository webCartRepository;
-
+	@Autowired
+	private IWebCartDetailRepository webCartDetailRepository;
 	@Override
 	public WebCart insert(WebCart t) {
 		return webCartRepository.save(t);
@@ -23,6 +27,15 @@ public class WebCartService implements IWebCartService {
 
 	@Override
 	public WebCart update(WebCart t) {
+		webCartDetailRepository.findByWebcartId(t.getId()).forEach(webCartDetail -> {
+			webCartDetailRepository.deleteById(webCartDetail.getId());
+		});
+		t.getWebCartDetails().forEach(webCartDetail -> {
+			webCartDetail.setId(null);
+			webCartDetailRepository.save(webCartDetail);
+		});
+
+
 		return webCartRepository.save(t);
 	}
 
@@ -38,11 +51,14 @@ public class WebCartService implements IWebCartService {
 
 	@Override
 	public List<WebCart> findAll() {
+		List<WebCart> wb=webCartRepository.findAll();
+
 		return webCartRepository.findAll();
 	}
 
 	@Override
 	public Optional<WebCart> findByUserId(int userId) {
+		Optional<WebCart> wb=webCartRepository.findByUserId(userId);
 		return webCartRepository.findByUserId(userId);
 	}
 
